ede5073a8c79fd7d7e6b6af992601abb8e7719d7
necmigunduz <necmigunduz@gmail.com>
2022-02-22 19:58:26 +0300

    Add model

---
+++ server/app/model.js
@@ -0,0 +1,21 @@
+// Weather model
+let mongoose = require('mongoose')
+// Setup Schema
+let productSchema = mongoose.Schema({
+    name: String,
+    brand: String,
+    desc: String,
+    color: String,
+    price: Number,
+    amount: {
+        type: Number,
+        default: 0
+    }
+})
+
+// Export the model
+let Product = module.exports = mongoose.model('product', productSchema)
+
+module.exports.get = function (callback, limit) {
+    Weather.find(callback).limit(limit)
+}
