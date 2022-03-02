b02965bb8f4c33cd9d8b4d3053aa680a46b30d75
necmigunduz <necmigunduz@gmail.com>
2022-03-02 12:46:55 +0300

    Transform App from a function to a class

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,68 @@
-import { useState, useEffect } from "react";
+import React, { Component } from "react";
 import Product from "./product/modal";
 import FetchData from "./api/fetchData";
 import "./App.css";
 
-function App() {
-  const [products, setProducts] = useState([]);
-  const getData = async () => {
-    let prdcts = await FetchData();
-    //console.log(prdcts.data)
-    setProducts(prdcts.data);
-  };
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
 
-  useEffect(() => {
-    getData();
-  }, [products]);
-  
-  return (
-<>
-<div className="App">
-      <table className="table table-hover">
-        <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Product Brand</th>
-          <th scope="col">Product Name</th>
-          <th scope="col">Product Details</th>
-        </tr>
-      </thead>
-      <tbody>
-       {products && products.map((product) =>{
-         return (
-           <tr key={product._id}>
-             <td>{products.indexOf(product)+1}</td>
-             <td>{product.brand}</td>
-             <td>{product.name}</td>
-             <td key={product.id}> 
-              <Product
-              pName={product.name}
-              pBrand={product.brand}
-              pColor={product.color}
-              pDesc={product.desc}
-              pPrice={product.price}
-             /></td>
-           </tr>
-         )
-       })} 
-       </tbody>
-      </table>
-    </div>
-    </>
-  );
+  componentDidMount() {
+    fetch("http://localhost:8080/api/products", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      mode: "cors"
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.data)
+        this.setState({ products: data.data });
+      })
+      .catch((error) => console.log(error.message));
+  }
+
+  render() {
+    return (
+      <>
+        <div className="App">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Product Brand</th>
+                <th scope="col">Product Name</th>
+                <th scope="col">Product Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.products.map((product) => {
+                  return (
+                    <tr key={product._id}>
+                      <td>{this.state.products.indexOf(product) + 1}</td>
+                      <td>{product.brand}</td>
+                      <td>{product.name}</td>
+                      <td key={product.id}>
+                        <Product
+                          pName={product.name}
+                          pBrand={product.brand}
+                          pColor={product.color}
+                          pDesc={product.desc}
+                          pPrice={product.price}
+                        />
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
